de5a0bef8b65161f923246aa584e8e837e9ffccb
Irusilda <irina.lyubodeeva@yandex.ru>
2023-02-10 12:06:23 +0300

    transition to SecondActivity

--- app/src/main/java/com/example/lyubodeeva_lesson2/MainActivity.kt
+++ app/src/main/java/com/example/lyubodeeva_lesson2/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.lyubodeeva_lesson2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnToSecond: Button = findViewById(R.id.firstActBtn)
+        val btnRussian: Button = findViewById(R.id.russianBtn)
+        val btnEnglish: Button = findViewById(R.id.englishBtn)
+        val textMain: TextView = findViewById(R.id.textView)
+
+        btnToSecond.setOnClickListener {
+            val myIntent = Intent(this, SecondActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
