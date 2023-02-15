fb09cef5c182e3009fd1b51db5aa2f900621bd46
Irusilda <irina.lyubodeeva@yandex.ru>
2023-02-15 15:14:18 +0300

    create layout and resurses

--- app/src/main/java/com/example/lyubodeeva_lesson2/MainActivity.kt
+++ app/src/main/java/com/example/lyubodeeva_lesson2/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.lyubodeeva_lesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.lyubodeeva_lesson2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.apply {
+            contactsBtn.setOnClickListener {
+                changeVisible()
+            }
+            vacanciesBtn.setOnClickListener {
+                changeVisible()
+            }
+            galleryBtn.setOnClickListener{
+                changeVisible()
+            }
+        }
+    }
+
+    private fun changeVisible() {
+        binding.apply {
+            groupForHide.visibility = View.GONE
+            progressBar.visibility = View.VISIBLE
+        }
+
     }
 }
